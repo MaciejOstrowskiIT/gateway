beaa81c903d58dbfded3b5ee1a9fe6693df2be5c
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2024-08-22 22:07:47 +0200

    project update

--- src/controllers/models/accountModel.ts
+++ src/controllers/models/accountModel.ts
@@ -1,16 +1,16 @@
 // import { ObjectId } from "mongodb";
 
-// export interface Account {
-// 	_id: ObjectId,
-// 	userId: ObjectId,
-// 	accountNumber: string,
-// 	profile: {
-// 		address: string,
-// 		fullName: string,
-// 		gender: string,
-// 		motherName: string,
-// 		NIP: string | null,
-// 		PESEL: string | null,
-// 		isCompanyAccount: boolean,
-// 	}
-// }
+export interface Account {
+  _id: string;
+  userId: string;
+  accountNumber: string;
+  profile: {
+    address: string;
+    fullName: string;
+    gender: string;
+    motherName: string;
+    NIP: string | null;
+    PESEL: string | null;
+    isCompanyAccount: boolean;
+  };
+}
--- src/controllers/models/userModel.ts
+++ src/controllers/models/userModel.ts
@@ -1,14 +1,14 @@
 // import { ObjectId } from "mongodb";
 
-// export interface User {
-// 	username: string,
-// 	password: string,
-// 	email: string,
-// 	accountId: null | ObjectId,
-// 	status: "ACTIVE" | "PENDING",
-// }
+export interface User {
+  username: string;
+  password: string;
+  email: string;
+  accountId: null | string;
+  status: 'ACTIVE' | 'PENDING';
+}
 
-// export interface LoginUser {
-// 	email: string,
-// 	password: string
-// }
+export interface LoginUser {
+  email: string;
+  password: string;
+}
